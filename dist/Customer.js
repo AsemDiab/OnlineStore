ade68168c73f4010f8f9f0daa37a5aea9fc7dbfd
AsemDiab <asemhesham55@gmail.com>
2026-02-15 18:46:54 +0200

    Delete js code

--- dist/Customer.js
+++
@@ -1,9 +0,0 @@
-import { User } from "./User.js";
-export class Customer extends User {
-    constructor() {
-        super();
-    }
-    getRole() {
-        return "Customer";
-    }
-}
--- dist/PriceValidator.js
+++
@@ -1,8 +0,0 @@
-export class PriceValidator {
-    constructor(minPrice = 0) {
-        this.minPrice = minPrice;
-    }
-    validate(price) {
-        return this.minPrice < price;
-    }
-}
