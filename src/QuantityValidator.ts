a81e5f531236c8b1e0598429230b3fc682de7343
AsemDiab <asemhesham55@gmail.com>
2026-02-16 13:01:40 +0200

    remove Validators Classes and depends on hleprs functions

--- src/QuantityValidator.ts
+++
@@ -1,7 +0,0 @@
-import { NumericalValidator } from "./NumericalValidator";
-
-export class QuantityValidator implements NumericalValidator {
-  validate(value: number): boolean {
-    return value > 0;
-  }
-}
